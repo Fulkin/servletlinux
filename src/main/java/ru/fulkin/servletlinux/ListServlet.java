51d21ea468dfac6a1c7792cb29a1f97669bdc4b1
Fulkin <sea-xv@mail.ru>
2022-06-17 16:22:29 +0500

    Initial commit

---
+++ src/main/java/ru/fulkin/servletlinux/ListServlet.java
@@ -0,0 +1,24 @@
+package ru.fulkin.servletlinux;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListServlet extends HttpServlet {
+    private static List<String> listName = Arrays.asList(
+            "Alex",
+            "Steven",
+            "Maria"
+            );
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("names", listName);
+        req.getRequestDispatcher("/listname.jsp").forward(req, resp);
+    }
+}
