e64e6985910b994fb7621388cdaa68e1000a1a08
Fulkin <sea-xv@mail.ru>
2022-06-19 15:07:13 +0500

    add names

--- src/main/java/ru/fulkin/servletlinux/ListServlet.java
+++ src/main/java/ru/fulkin/servletlinux/ListServlet.java
@@ -12,7 +12,9 @@ public class ListServlet extends HttpServlet {
     private static List<String> listName = Arrays.asList(
             "Alex",
             "Steven",
-            "Maria"
+            "Maria",
+            "Kate",
+            "Eugene"
             );
 
 
